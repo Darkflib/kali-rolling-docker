a4edd67a72447c595fbee852238ee984d429a7b1
Mike Preston <darkflib@gmail.com>
2018-11-27 16:32:01 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+DOCKERREPO=darkflib/kali-linux-rolling
 # Install dependencies (debootstrap)
 sudo apt-get install debootstrap
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;hb=HEAD" > kali-debootstrap &&\
-	sudo debootstrap kali-rolling ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
-	# Import the Kali image into Docker
-sudo tar -C kali-root -c . | sudo docker import - darkflib/kali-linux-rolling &&\
-	sudo rm -rf ./kali-root &&\
-	# Test the Kali Docker Image
-docker run -t -i kalilinux/kali cat /etc/debian_version &&\
-	echo "Build OK" || echo "Build failed!"
-
+        sudo debootstrap kali-rolling ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
+        # Import the Kali image into Docker
+echo deb http://http.kali.org/kali kali-rolling main contrib non-free > ./kali-root/etc/apt/sources.list
+sudo tar -C kali-root -c . | sudo docker import - ${DOCKERREPO} &&\
+        # sudo rm -rf ./kali-root # &&\
+        # Test the Kali Docker Image
+docker run -t -i ${DOCKERREPO} cat /etc/debian_version &&\
+        echo "Build OK" || echo "Build failed!"
