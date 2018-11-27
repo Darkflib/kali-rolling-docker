f135110a83660e9d01c800bd46d7b87255c51439
Mike Preston <darkflib@gmail.com>
2018-11-27 23:43:37 +0000

    initial test of metasploit branch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKERREPO=darkflib/kali-linux-rolling
+DOCKERREPO=darkflib/kali-linux-rolling-metasploit
 # Install dependencies (debootstrap)
 sudo apt-get install debootstrap
 # Fetch the latest Kali debootstrap script from git
@@ -7,8 +7,27 @@ curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scri
         sudo debootstrap kali-rolling ./kali-root http://http.kali.org/kali ./kali-debootstrap &&\
         # Import the Kali image into Docker
 echo deb http://http.kali.org/kali kali-rolling main contrib non-free > ./kali-root/etc/apt/sources.list
-sudo tar -C kali-root -c . | sudo docker import - ${DOCKERREPO} &&\
-        # sudo rm -rf ./kali-root # &&\
-        # Test the Kali Docker Image
-docker run -t -i ${DOCKERREPO} cat /etc/debian_version &&\
+
+# because we want to install from potentially multiple repos, we can use either multistrap or run a script.
+# I choose the latter.
+
+cat << EOF > ./kali-root/bootstrap.sh
+#!/bin/bash
+
+echo 'Running bootstrap script'
+
+apt update
+apt install metaspoit
+echo 'Please run msfupdate to update the framework' >> /etc/motd
+apt clean
+EOF
+
+sudo tar -C kali-root -c . | sudo docker import - ${DOCKERREPO}
+# sudo rm -rf ./kali-root # &&\
+   
+# run the script
+docker run -it ${DOCKERREPO} /bin/bash /bootstrap.sh
+
+# Test the Kali Docker Image
+docker run -it ${DOCKERREPO} cat /etc/debian_version &&\
         echo "Build OK" || echo "Build failed!"
